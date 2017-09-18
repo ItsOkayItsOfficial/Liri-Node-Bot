ae12e6e89626954f528f085840a442afb80b59d0
Alex P <alexp@itsokayitsofficial.io>
2017-09-18 13:17:18 -0700

    Added keys.js, liri.js, random.txt and updated .gitignore

---
+++ keys.js
@@ -0,0 +1,19 @@
+/*
+* Author: Alex P
+* Project Name: liri-node-app
+* Version: 1
+* Date: 09.18.17
+* URL: github.com/itsokayitsofficial/liri-node-app
+*/
+
+// Twitter API keys
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'LRtKWnfkEYctrT5udepDGADyM',
+  consumer_secret: 'gv1RiB4eXd4mftJJKYnAS7BC7SrI0d7IjY9nQn8MRwRC9Q7F7G',
+  access_token_key: '2823395241-uYjfYfpdBkneGSuUD0G5xBmWrXDZKsVrFPrhGhT',
+  access_token_secret: 'RTe0Oh7TY4qMyCWocFUA5OktmbiInnE1kwwiv6OAzCYBb',
+}
+
+module.exports = twitterKeys;
