d82007c969ab8a1a2dd2372703eea23d8748bff1
Alex P <alexp@itsokayitsofficial.io>
2017-09-19 15:40:27 -0700

    Added log.txt, updated keys.js, package-lock.json, package.json, and added logging to liri.js

--- keys.js
+++ keys.js
@@ -1,19 +1,27 @@
 /*
 * Author: Alex P
-* Project Name: liri-node-app
+* Project Name: liri-node-app keys.js
 * Version: 1
 * Date: 09.18.17
 * URL: github.com/itsokayitsofficial/liri-node-app
 */
 
 // Twitter API keys
-console.log('this is loaded');
-
 var twitterKeys = {
   consumer_key: 'LRtKWnfkEYctrT5udepDGADyM',
   consumer_secret: 'gv1RiB4eXd4mftJJKYnAS7BC7SrI0d7IjY9nQn8MRwRC9Q7F7G',
   access_token_key: '2823395241-uYjfYfpdBkneGSuUD0G5xBmWrXDZKsVrFPrhGhT',
   access_token_secret: 'RTe0Oh7TY4qMyCWocFUA5OktmbiInnE1kwwiv6OAzCYBb',
-}
+};
 
-module.exports = twitterKeys;
+// Spotify API keys
+var spotifyKeys = {
+  client_id: '21ff2fb0d1594b44b2759fdcdd60b77d',
+  client_secret: '4a8f6c3b93ea44b7a465ad51f2128704',
+};
+
+// Exports
+module.exports = {
+  twitterKeys,
+  spotifyKeys,
+};
